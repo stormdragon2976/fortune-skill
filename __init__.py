03c95568bdd4fd1cb5ae82c1eed72cf28b1b35ec
Storm Dragon <stormdragon2976@gmail.com>
2020-07-15 16:37:58 -0400

    More progress on actually getting a fortune.

--- __init__.py
+++ __init__.py
@@ -5,9 +5,12 @@ import subprocess
 class Fortune(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        super().__init__()
+        self.learning = True
 
     @intent_file_handler('fortune.intent')
-    def handle_fortune(self, message):
+
+    def handle_fortune_intent(self, message):
         fortune = subprocess.check_output(["fortune"])
         for line in fortune.strip().decode().splitlines():
             self.speak_dialog('fortune')
